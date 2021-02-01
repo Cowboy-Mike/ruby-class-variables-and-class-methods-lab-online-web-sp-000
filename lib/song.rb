c291f027a1edb58e70280a2c9f827ea9368d93db
Mike Nanto <mike@thecowboymechanic.com>
2021-02-01 07:06:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,5 +29,13 @@ end
 
 def self.genre_count
   genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1 
+    else
+      genre_count[genre] = 1 
+    end
+    genre_count
+  end
   
 end
