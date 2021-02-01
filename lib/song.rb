6c9b8e2b7653f0de6bd6300cc33fde9edb25bb1f
Mike Nanto <mike@thecowboymechanic.com>
2021-02-01 06:53:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ attr_accessor :name, :artist, :genre
 
   def initialize (song_name, artist_name, genre_type)
   @artist = artist_name
-  @song = song_name
+  @name = song_name
   @genre = genre_type
   end
 
